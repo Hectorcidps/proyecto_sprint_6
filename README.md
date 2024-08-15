93eee2822da675a25fddcbe2f85531dddca99dc3
Hector Cid del Prado <hectorciddelprado@Hectors-MacBook-Pro.local>
2024-08-15 11:49:44 -0600

    primera revision: App web

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# proyecto_sprint_6
+## Proyecto sprint 6
+
+Este proyecto es mi primera app web desarrollada con streamlit y plotly_express, donde se pueden generar dos gráficos, el primero es un histograma y el segundo es un gráfico de dispersión, los cuales muestran la información de un conjunto de datos de anuncios de venta de coches.
+
+[Enlace al repositorio en GitHub](https://github.com/Hectorcidps/proyecto_sprint_6)
+
+URL de la app en Render
+
+
+
---
+++ app.py
@@ -0,0 +1,38 @@
+import pandas as pd 
+import plotly.express as px 
+import streamlit as st 
+
+# leer el CSV con la informacion necesaria 
+car_data = pd.read_csv('./vehicles_us.csv')
+
+# Crear un encabezado 
+st.header('Anuncios de ventas de coches')
+
+# Crear un checkbox para construir un histograma 
+hist_checkbox = st.checkbox('Ver Histograma')
+
+if hist_checkbox:
+    # Mensaje al presionar el boton 'Ver Histograma'
+    st.write('Creación de un histograma con la información del conjunto de datos de anuncios de venta de coches')
+    
+    # crear el histograma 
+    histo = px.histogram(car_data, x='odometer')
+    
+    # mostrar el grafico 
+    st.plotly_chart(histo, use_container_width=True)
+
+
+# Crear un checkbox para construir un grafico de dispersion 
+scatter_checkbox = st.checkbox('Ver Gráfico de dispersión')
+
+if scatter_checkbox:
+    st.write('Creación de un gráfico de dispersión con la información del conjunto de datos de anuncios de venta de coches')
+    
+    # crear el scatter
+    scatter = px.scatter(car_data,
+                         x='odometer',
+                         y='price',
+                         color_continuous_scale='reds')
+    
+    # mostrar el grafico 
+    st.plotly_chart(scatter, use_container_width=True )
